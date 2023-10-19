6cffbbb31576cbc30319f97032b6caf212500057
bibodev23 <bibodev23@gmail.com>
2023-10-19 08:53:40 +0200

    responsive smartphone

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -12,3 +12,24 @@ function enqueue_custom_js()
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_custom_js');
+
+
+
+add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
+
+function add_admin_link( $items, $args ) {
+
+    if (is_user_logged_in() && $args->theme_location == 'main_menu') {
+        $menu_items = explode('</li>', $items);
+        if (count($menu_items) >= 2) {
+            // Insérez le lien "Admin" entre les deux parties
+            $admin_link = '<li class="lien_admin"><a href="' . get_admin_url() . '">Admin</a></li>';
+            $items = $menu_items[0] . $admin_link . '</li>' . $menu_items[1] . '</li>';
+        }
+    }
+
+    return $items;
+
+}
+
+?>
